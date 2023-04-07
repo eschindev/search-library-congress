24764024d3c556a688ebf552803c635c9fa672c3
David C <dchung20315@gmail.com>
2023-04-07 14:04:23 -0700

    added fetch function

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -2,3 +2,20 @@ var searchBtnEl = $("search-btn");
 var searchBoxEl = $("search-box");
 var searchFormatEl = $("search-format");
 var outputEl = $("output-col");
+
+var getLibraryContent = function (searchTerm, format) {
+    var apiUrl = `https://www.loc.gov/${format}/?q=${searchTerm}&fo=json`
+  
+    fetch(apiUrl)
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+          
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to Library of Congress');
+      });
+  };
