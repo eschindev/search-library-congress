8cd0330b23d0cb6ed434bc3209efb1f89e1911b5
David C <dchung20315@gmail.com>
2023-04-07 14:10:38 -0700

    added a function for the submit form

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -3,6 +3,21 @@ var searchBoxEl = $("search-box");
 var searchFormatEl = $("search-format");
 var outputEl = $("output-col");
 
+//
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+  
+    var searchTerm = searchBoxEl.value.trim();
+    var format = ;
+
+    if (searchTerm) {
+      getLibraryContent(searchTerm, format)
+    } else {
+      alert('Please enter a search term and format');
+    }
+  };
+
+ //fetch content from LoC api 
 var getLibraryContent = function (searchTerm, format) {
     var apiUrl = `https://www.loc.gov/${format}/?q=${searchTerm}&fo=json`
   
@@ -17,5 +32,11 @@ var getLibraryContent = function (searchTerm, format) {
       })
       .catch(function (error) {
         alert('Unable to connect to Library of Congress');
-      });
+      })
+
+      .then(function (placeholder) {
+        //generate the contents that were searched based on format and searchTerm
+
+      })
   };
+
